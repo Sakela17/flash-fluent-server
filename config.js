ebc136a384b50412b89307d34e84c99dce2e01f2
Marina Valiquette <marina.valiquette@gmail.com>
2018-07-15 13:11:01 -0400

    Add buildings data for db seed

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ module.exports = {
   CLIENT_ORIGIN     : process.env.CLIENT_ORIGIN || 'http://localhost:3000',
   DATABASE_URL      : process.env.DATABASE_URL,
   TEST_DATABASE_URL : process.env.TEST_DATABASE_URL,
-  JWT_SECRET: process.env.JWT_SECRET,
-  JWT_EXPIRY: process.env.JWT_EXPIRY || '7d'
+  JWT_SECRET        : process.env.JWT_SECRET,
+  JWT_EXPIRY        : process.env.JWT_EXPIRY || '7d'
 };
